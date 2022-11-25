ff58ea5752367e7c8ad81212673cd04f5b6e45ef
eleon479 <eleon479@gmail.com>
2022-11-25 13:24:33 -0500

    add post templates and mock data

--- src/App.js
+++ src/App.js
@@ -7,21 +7,73 @@ import {
   FaRegEnvelope,
   FaRegUser,
   FaHome,
+  FaRegEdit,
+  FaEllipsisH,
+  FaSearch,
+  FaImage,
+  FaRegComment,
+  FaRetweet,
 } from "react-icons/fa";
+import { RiFileGifLine } from "react-icons/ri";
+import {
+  MdOutlinePoll,
+  MdSchedule,
+  MdOutlineLocationOn,
+  MdOutlineFavoriteBorder,
+} from "react-icons/md";
+import { BiHappyBeaming } from "react-icons/bi";
+import { FiShare } from "react-icons/fi";
+import { AiOutlineRetweet } from "react-icons/ai";
+import GetTimelineFeed from "./tweets";
+
+function MakePost(name, tag, ts, text) {
+  return (
+    <div className="Post">
+      <div className="post-pfp-container">
+        <FaRegUser className="post-pfp-icon" />
+      </div>
+      <div className="post-contents-container">
+        <div className="post-info">
+          <div className="poster-name">{name}</div>
+          <div className="poster-tag">@{tag}</div>
+          <div className="time-since"> · {ts}</div>
+        </div>
+        <div className="post-text">{text}</div>
+        <div className="post-actions">
+          <div className="reply-action">
+            <FaRegComment className="post-action" />
+          </div>
+          <div className="retweet-action">
+            <AiOutlineRetweet className="post-action" />
+          </div>
+          <div className="like-action">
+            <MdOutlineFavoriteBorder className="post-action" />
+          </div>
+          <div className="share-action">
+            <FiShare className="post-action" />
+          </div>
+        </div>
+      </div>
+      <div className="post-options">
+        <FaEllipsisH />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <div className="App-wrapper">
         <aside id="Navigation">
-          <a href="https://twitter.com">
+          <a href="/">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper bird-logo">
                 <FaTwitter />
               </div>
             </div>
           </a>
-          <a href="https://twitter.com" className="Active-tab">
+          <a href="/home" className="Active-tab">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper">
                 <FaHome />
@@ -31,7 +83,7 @@ function App() {
               </div>
             </div>
           </a>
-          <a href="https://twitter.com">
+          <a href="/explore">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper">
                 <FaHashtag />
@@ -41,7 +93,7 @@ function App() {
               </div>
             </div>
           </a>
-          <a href="https://twitter.com">
+          <a href="/notis">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper">
                 <FaRegBell />
@@ -51,17 +103,17 @@ function App() {
               </div>
             </div>
           </a>
-          <a href="https://twitter.com">
+          <a href="/dms">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper">
                 <FaRegEnvelope />
               </div>
               <div className="Navigation-label-wrapper">
-                <span>Message</span>
+                <span>Messages</span>
               </div>
             </div>
           </a>
-          <a href="https://twitter.com">
+          <a href="/profile">
             <div className="Navigation-link-inner">
               <div className="Navigation-icon-wrapper">
                 <FaRegUser />
@@ -71,16 +123,36 @@ function App() {
               </div>
             </div>
           </a>
-          <a href="https://twitter.com" className="Navigation-tweet-btn-sm">
-            T
-          </a>
-          <a href="https://twitter.com" className="Navigation-tweet-btn-lg">
+          <a href="/new" className="Navigation-tweet-btn-lg">
             <div className="Navigation-tweet-btn-inner">
               <span className="Navigation-tweet-btn-label">Tweet</span>
             </div>
           </a>
-        </aside>
+          <a href="/new" className="Navigation-tweet-btn-sm">
+            <div className="Navigation-tweet-btn-inner">
+              <span className="Navigation-tweet-btn-icon">
+                <FaRegEdit className="write-icon" />
+              </span>
+            </div>
+          </a>
 
+          <div className="Account-switch-container">
+            <div className="Account-switch-wrapper">
+              <div className="Account-switch-pfp-container">
+                <FaRegUser className="Account-switch-pfp" />
+              </div>
+              <div className="Account-switch-details-container">
+                <div className="Account-switch-details-inner">
+                  <div className="Account-switch-name">eddy leon</div>
+                  <div className="Account-switch-tag">@eleon479</div>
+                </div>
+              </div>
+              <div className="Account-switch-options">
+                <FaEllipsisH style={{ fontWeight: 300 }} />
+              </div>
+            </div>
+          </div>
+        </aside>
         <main id="Main">
           <nav>
             <div id="Main-nav-inner">
@@ -89,7 +161,6 @@ function App() {
               </div>
             </div>
           </nav>
-
           <div className="Post-feed">
             <div id="Write-tweet">
               <div className="pfp-area">
@@ -105,12 +176,15 @@ function App() {
                 </div>
                 <div id="Write-tweet-actions">
                   <div id="Write-tweet-tools">
-                    <FaHashtag className="tool" />
-                    <FaRegBell className="tool" />
-                    <FaRegUser className="tool" />
+                    <FaImage className="tool" />
+                    <RiFileGifLine className="tool" />
+                    <MdOutlinePoll className="tool" />
+                    <BiHappyBeaming className="tool" />
+                    <MdSchedule className="tool" />
+                    <MdOutlineLocationOn className="tool" />
                   </div>
                   <div id="Write-tweet-submit-wrapper">
-                    <a href="https://twitter.com" id="Write-tweet-submit-btn">
+                    <a href="/send" id="Write-tweet-submit-btn">
                       <div id="Write-tweet-submit-btn-inner">
                         <span id="Write-tweet-submit-btn-label">Tweet</span>
                       </div>
@@ -120,47 +194,37 @@ function App() {
               </div>
             </div>
 
-            <div className="Post">
-              <div className="pfp-area">
-                <FaRegUser className="pfp-icon" />
-              </div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </div>
-            <div className="Post">
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </div>
-            <div className="Post">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur.
-            </div>
-            <div className="Post">
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum.
-            </div>
-            <div className="Post">
-              Bibendum neque egestas congue quisque egestas.
-            </div>
-            <div className="Post">
-              Malesuada nunc vel risus commodo viverra maecenas accumsan lacus
-              vel. Urna neque viverra justo nec ultrices dui sapien.
-            </div>
-            <div className="Post">
-              Consectetur adipiscing elit pellentesque habitant morbi tristique
-              senectus et.
-            </div>
-            <div className="Post">Post</div>
+            {/* Tweet Feed */}
+            {GetTimelineFeed().map((tweet) =>
+              MakePost(tweet.name, tweet.tag, tweet.ts, tweet.text)
+            )}
           </div>
         </main>
-
         <aside id="Explore">
-          <ul>
-            <li>#Yes</li>
-            <li>#No</li>
-            <li>#Maybe</li>
-          </ul>
+          <div id="search-container">
+            <div id="search-wrapper">
+              <div id="search-icon-wrapper">
+                <FaSearch className="input-icon" />
+              </div>
+              <div id="search-input-wrapper">
+                <input
+                  id="search-input"
+                  placeholder={"Search Twitter"}
+                  type={"text"}
+                  maxLength={27}
+                />
+              </div>
+            </div>
+          </div>
+          <div id="explore-container">
+            <div id="trending">
+              <ul>
+                <li>#ByeTwitter</li>
+                <li>#GrimesComeBack</li>
+                <li>#IllTakePeteDavidson</li>
+              </ul>
+            </div>
+          </div>
         </aside>
       </div>
     </div>
---
+++ src/tweets.js
@@ -0,0 +1,69 @@
+export default function GetTimelineFeed() {
+  const tweets = [
+    {
+      name: "Certified Sh*tposter",
+      tag: "spicymemes69",
+      ts: "12h",
+      text: "This is the first real tweet!",
+    },
+    {
+      name: "Memelord Supreme",
+      tag: "r3dditl4d",
+      ts: "15m",
+      text: "Boy I sure hope Mr. Musk doesnt buy this too! ;)",
+    },
+    {
+      name: "Hispanick Jonas",
+      tag: "sailingburrito",
+      ts: "58m",
+      text: "ARGENTINA WHAT ARE YOU DOING??? brb burning my messi shirt",
+    },
+    {
+      name: "I Ran Out of Names",
+      tag: "notcreative",
+      ts: "4h",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
+        "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut" +
+        "enim ad minim veniam",
+    },
+    {
+      name: "Test1",
+      tag: "tst123",
+      ts: "10s",
+      text: "Pls don't blow up...",
+    },
+    {
+      name: "Test2",
+      tag: "tst123",
+      ts: "5s",
+      text: "Pls don't blow up...",
+    },
+    {
+      name: "Test3",
+      tag: "tst123",
+      ts: "17s",
+      text: "Pls don't blow up...",
+    },
+    {
+      name: "Test4",
+      tag: "tst123",
+      ts: "2s",
+      text: "Pls don't blow up...",
+    },
+    {
+      name: "Test5",
+      tag: "tst123",
+      ts: "9s",
+      text: "Pls don't blow up...",
+    },
+    {
+      name: "Test6",
+      tag: "tst123",
+      ts: "12s",
+      text: "Pls don't blow up...",
+    },
+  ];
+
+  return tweets;
+}
